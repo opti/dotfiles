a29adc8dca9de1623a5a252429c33af3072898ed
Igor Pstyga <i.pstuga@guideline.com>
2025-11-03 15:50:59 -0800

    fix(nvim): resolve warning for avante config

--- nvim/lua/opti/plugins/avante.lua
+++ nvim/lua/opti/plugins/avante.lua
@@ -2,25 +2,31 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  version = false, -- 1) "*" - latest release version, 2) false - latest code changes.
   opts = {
     -- add any opts here
     -- for example
     provider = "claude",
-    claude = {
-      endpoint = "https://api.anthropic.com",
-      model = "claude-3-7-sonnet-20250219",
-      temperature = 0,
-      max_tokens = 4096,
+    providers = {
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-7-sonnet-20250219",
+        extra_request_body = {
+          temperature = 0,
+          max_tokens = 4096,
+        },
+      },
+      -- openai = {
+      --   endpoint = "https://api.openai.com/v1",
+      --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      --   timeout = 30000, -- timeout in milliseconds
+      --   extra_request_body = {
+      --     temperature = 0, -- adjust if needed
+      --     max_tokens = 4096,
+      --   }
+      --   -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      -- },
     },
-    -- openai = {
-    --   endpoint = "https://api.openai.com/v1",
-    --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-    --   timeout = 30000, -- timeout in milliseconds
-    --   temperature = 0, -- adjust if needed
-    --   max_tokens = 4096,
-    --   -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
-    -- },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
